d23e56c1007db26174321f0d95a973432f1a3656
Gabor Heja <gheja@m002560p>
2018-08-08 01:52:37 +0200

    GameObjectPlayerOne: reworked bar drawing

--- src/game_object_player_one.js
+++ src/game_object_player_one.js
@@ -66,35 +66,41 @@ class GameObjectPlayerOne extends GameObject
 	
 	drawBars()
 	{
-		let c;
+		let c, x, y, reversed;
+		
+		x = 0;
+		y = 46;
+		reversed = false;
 		
 		c = PLAYER_HIGHLIGHT_COLORS[this.playerIndex];
 		
 		if (this.playerIndex == 0)
 		{
-			_gfx.drawVerticalBar(0, 46, 2, 18, this.power / this.stats.powerMax, { h: 0, s: 1, l: 1 });
-			_gfx.drawVerticalBar(2, 52, 2, 12, this.armor / this.stats.armorMax, { h: 15, s: 0.5, l: 0.7 });
-			_gfx.drawVerticalBar(4, 52, 2, 12, this.shield / this.stats.shieldMax, { h: 200, s: 1, l: 1 });
-			
-			if (this.highlightTicksLeft > 0)
-			{
-				_gfx.drawBox(0, 62, 6, 2, c);
-			}
+			x = 0;
 		}
 		else
 		{
-			_gfx.drawVerticalBar(62, 46, 2, 18, this.power / this.stats.powerMax, { h: 0, s: 1, l: 1 });
-			_gfx.drawVerticalBar(60, 52, 2, 12, this.armor / this.stats.armorMax, { h: 15, s: 0.5, l: 0.7 });
-			_gfx.drawVerticalBar(58, 52, 2, 12, this.shield / this.stats.shieldMax, { h: 200, s: 1, l: 1 });
-			
-			if (this.highlightTicksLeft > 0)
-			{
-				_gfx.drawBox(58, 62, 14, 2, c);
-			}
+			x = 58;
+			reversed = true;
 		}
 		
-		if (this.highlightTicksLeft > 0)
+		if (!reversed)
 		{
+			_gfx.drawVerticalBar(x, y, 2, 18, this.power / this.stats.powerMax, { h: 0, s: 1, l: 1 });
+			_gfx.drawVerticalBar(x + 2, y + 6, 2, 12, this.armor / this.stats.armorMax, { h: 15, s: 0.5, l: 0.7 });
+			_gfx.drawVerticalBar(x + 4, y + 6, 2, 12, this.shield / this.stats.shieldMax, { h: 200, s: 1, l: 1 });
+			
+		}
+		else
+		{
+			_gfx.drawVerticalBar(x + 4, y, 2, 18, this.power / this.stats.powerMax, { h: 0, s: 1, l: 1 });
+			_gfx.drawVerticalBar(x + 2, y + 6, 2, 12, this.armor / this.stats.armorMax, { h: 15, s: 0.5, l: 0.7 });
+			_gfx.drawVerticalBar(x, y + 6, 2, 12, this.shield / this.stats.shieldMax, { h: 200, s: 1, l: 1 });
+		}
+		
+		if (this.highlightTicksLeft > 0 && (_game.ticks % 15 < 10))
+		{
+			_gfx.drawBox(x, y + 16, 6, 2, c);
 			_gfx.drawBox(this.screenX - 2, this.screenY - 8, 5, 1, c);
 			_gfx.drawBox(this.screenX - 1, this.screenY - 7, 3, 1, c);
 			_gfx.drawBox(this.screenX, this.screenY - 6, 1, 1, c);
