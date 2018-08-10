1ef2d852ff703b93c3ca9097cc247ab494f83ae9
Gabor Heja <gheja@m002560p>
2018-08-10 08:52:11 +0200

    InputKeyboard2: added shoot and menu keys

--- src/input_keyboard_2.js
+++ src/input_keyboard_2.js
@@ -10,5 +10,7 @@ class InputKeyboard2 extends InputKeyboard
 		this.keys["right"].codes = [ "d", "D", "KeyD" ];
 		this.keys["down"].codes = [ "s", "S", "KeyS" ];
 		this.keys["left"].codes = [ "a", "A", "KeyA" ];
+		this.keys["shoot"].codes = [ "Shift" ];
+		this.keys["menu"].codes = [ "q", "Q", "KeyQ" ];
 	}
 }
