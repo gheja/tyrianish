8352f583d6de8347f246d7a3d91878b239e996bf
Gabor Heja <gheja@m002560p>
2018-08-12 00:50:27 +0200

    Added more debug options

--- src/debug.js
+++ src/debug.js
@@ -1,5 +1,14 @@
 "use strict";
 
 let _debug = {
-	hitboxes: false
+	hitboxes: false,
+	paths: false,
+	paused: false,
+	slow: false,
+	slowCounter: 0,
+	editing: false,
+	editX: 4,
+	editY: 100,
+	editHoveredObject: null,
+	editSelectedObject: null
 };
