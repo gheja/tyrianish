b6029100e55fbf00e71e0a0ad9d8b792c20a3566
Gabor Heja <gheja@m002560p>
2018-08-12 00:56:55 +0200

    GameObjectPlayerOne: added handling of player status, added left-right tilt graphics

--- src/game_object_player_one.js
+++ src/game_object_player_one.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const PLAYER_STATUS_NEW = 0;
+const PLAYER_STATUS_PLAYING = 1;
+const PLAYER_STATUS_DYING = 2;
+const PLAYER_STATUS_DEAD = 3;
+
 class GameObjectPlayerOne extends GameObjectShip
 {
 	constructor(settings)
@@ -10,7 +15,8 @@ class GameObjectPlayerOne extends GameObjectShip
 		
 		this.hitCheckEnabled = true;
 		this.hitCheckGroup = 1;
-		this.highlightTicksLeft = 60;
+		this.highlightTicksLeft = 0;
+		this.startTicksLeft = 0;
 		this.playerIndex = 0;
 		this.shootTicksLeft = 0;
 		
@@ -22,16 +28,54 @@ class GameObjectPlayerOne extends GameObjectShip
 		this.stats.shieldRechargeSpeed = 0.2;
 		this.stats.armorRechargeSpeed = 0;
 		
+		this.status = PLAYER_STATUS_NEW;
+		
 		this.input = _inputs[0];
+		this.explosionAnimations = [ "explosion1" ];
+		this.dead = false;
+		
+		this.restart();
 		
 		_merge(this, settings);
+	}
+	
+	handleHitBy(projectile)
+	{
+		if (this.status != PLAYER_STATUS_PLAYING)
+		{
+			return;
+		}
+		
+		super.handleHitBy(projectile);
 	}
 	
 	shoot()
 	{
 		this.shootTicksLeft = 10;
 		this.power -= 0.2;
-		_game.objects.push(new GameObjectProjectile({ screenX: this.screenX, screenY: this.screenY, speedX: 0, speedY: -3, hitCheckGroup: this.hitCheckGroup }));
+		_game.objects.push(new GameObjectProjectile({ screenX: this.screenX, screenY: this.screenY - 10, speedX: 0, speedY: -3, hitCheckGroup: this.hitCheckGroup }));
+	}
+	
+	restart()
+	{
+		this.highlightTicksLeft = 60;
+		this.startTicksLeft = 60;
+		this.armor = 0.5;
+		this.shield = 0.8;
+		this.status = PLAYER_STATUS_NEW;
+	}
+	
+	destroy()
+	{
+		if (this.status != PLAYER_STATUS_PLAYING)
+		{
+			return;
+		}
+		
+		super.destroy();
+		
+		this.destroyed = false;
+		this.status = PLAYER_STATUS_DEAD;
 	}
 	
 	tick()
@@ -48,12 +92,51 @@ class GameObjectPlayerOne extends GameObjectShip
 			this.highlightTicksLeft--;
 		}
 		
-		this.rechargeTick();
+		if (this.status == PLAYER_STATUS_NEW)
+		{
+			if (this.startTicksLeft > 0)
+			{
+				this.startTicksLeft--
+			}
+			else if (this.startTicksLeft == 0)
+			{
+				this.status = PLAYER_STATUS_PLAYING;
+			}
+		}
 		
 		a = this.input.query();
 		
 		this.speedX = clamp(-this.speedMaxX, this.speedMaxX, this.speedX + a.x * 0.15);
 		this.speedY = clamp(-this.speedMaxY, this.speedMaxY, this.speedY + a.y * 0.15);
+		
+		if (this.status == PLAYER_STATUS_DEAD)
+		{
+			// this.gfxObject = _gfxObjects.get("explosion1");
+			this.gfxObject = _gfxObjects.get("player1_dead");
+		}
+		else
+		{
+			if (this.speedX < -0.8)
+			{
+				this.gfxObject = _gfxObjects.get("player1_left2");
+			}
+			else if (this.speedX < -0.5)
+			{
+				this.gfxObject = _gfxObjects.get("player1_left");
+			}
+			else if (this.speedX <= 0.5)
+			{
+				this.gfxObject = _gfxObjects.get("player1");
+			}
+			else if (this.speedX <= 0.8)
+			{
+				this.gfxObject = _gfxObjects.get("player1_right");
+			}
+			else
+			{
+				this.gfxObject = _gfxObjects.get("player1_right2");
+			}
+		}
 		
 		this.speedX *= this.speedReduction;
 		this.speedY *= this.speedReduction;
@@ -61,16 +144,27 @@ class GameObjectPlayerOne extends GameObjectShip
 		this.screenX = clamp(0, WIDTH, this.screenX + this.speedX);
 		this.screenY = clamp(0, HEIGHT, this.screenY + this.speedY);
 		
-		if (this.shootTicksLeft == 0)
+		if (this.status == PLAYER_STATUS_DEAD)
 		{
-			if (a.shoot)
-			{
-				this.shoot();
-			}
+			this.power = 0;
+			this.shield = 0;
+			this.armor = 0;
 		}
 		else
 		{
-			this.shootTicksLeft--;
+			this.rechargeTick();
+			
+			if (this.shootTicksLeft == 0)
+			{
+				if (a.shoot)
+				{
+					this.shoot();
+				}
+			}
+			else
+			{
+				this.shootTicksLeft--;
+			}
 		}
 	}
 	
